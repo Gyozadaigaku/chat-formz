8f1ca720067384f3b85cdfc014c7893499b35776
Gyozadaigaku <gaspardcube15@gmail.com>
2024-06-29 17:52:33 +0900

    📦 Add dndkit

--- components/designer.tsx
+++ components/designer.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import DesignerSidebar from "./designer-sidebar";
+import { useDroppable } from "@dnd-kit/core";
 
 export default function Designer() {
+  const droppable = useDroppable({
+    id: "designer-drop-area",
+    data: {
+      isDesignerDropArea: true,
+    },
+  });
+
   return (
     <div className="flex h-full w-full">
       <div className="w-full p-4">
--- components/form-builder.tsx
+++ components/form-builder.tsx
@@ -3,26 +3,29 @@
 import { Form } from "@prisma/client";
 import SaveFormBtn from "./save-form-btn";
 import Designer from "./designer";
+import { DndContext } from "@dnd-kit/core";
 
 export default function FormBuilder({ form }: { form: Form }) {
   return (
-    <main className="flex w-full flex-col">
-      <nav className="flex items-center justify-between gap-3 border-b-2 p-4">
-        <h2 className="truncate font-medium">
-          <span className="mr-2 text-muted-foreground">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex items-center gap-2">
-          {!form.published && (
-            <>
-              <SaveFormBtn id={form.id} />
-            </>
-          )}
+    <DndContext>
+      <main className="flex w-full flex-col">
+        <nav className="flex items-center justify-between gap-3 border-b-2 p-4">
+          <h2 className="truncate font-medium">
+            <span className="mr-2 text-muted-foreground">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            {!form.published && (
+              <>
+                <SaveFormBtn id={form.id} />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="relative flex h-[200px] w-full flex-grow items-center justify-center overflow-y-auto bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
         </div>
-      </nav>
-      <div className="relative flex h-[200px] w-full flex-grow items-center justify-center overflow-y-auto bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
-        <Designer />
-      </div>
-    </main>
+      </main>
+    </DndContext>
   );
 }

e4dd6c40a2afed61a81b8e187fbf016410f18398
Gyozadaigaku <gaspardcube15@gmail.com>
2024-06-29 17:33:50 +0900

    💄 Add pattern

--- components/form-builder.tsx
+++ components/form-builder.tsx
@@ -1,5 +1,25 @@
 "use client";
 
-export default function FormBuilder() {
-  return <div>FormBuilder</div>;
+import { Form } from "@prisma/client";
+import SaveFormBtn from "./save-form-btn";
+
+export default function FormBuilder({ form }: { form: Form }) {
+  return (
+    <main className="flex w-full flex-col">
+      <nav className="flex items-center justify-between gap-3 border-b-2 p-4">
+        <h2 className="truncate font-medium">
+          <span className="mr-2 text-muted-foreground">Form:</span>
+          {form.name}
+        </h2>
+        <div className="flex items-center gap-2">
+          {!form.published && (
+            <>
+              <SaveFormBtn id={form.id} />
+            </>
+          )}
+        </div>
+      </nav>
+      <div className="relative flex h-[200px] w-full flex-grow items-center justify-center overflow-y-auto bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]"></div>
+    </main>
+  );
 }

d856f6979c3cada4b60f8ec2359a7a95f7ed65a8
Gyozadaigaku <gaspardcube15@gmail.com>
2024-07-01 19:36:55 +0900

    ✨ Add form properties

--- components/designer-sidebar.tsx
+++ components/designer-sidebar.tsx
@@ -1,11 +1,14 @@
-import { FormElements } from "./form-elements";
-import SidebarBtnElement from "./sidebar-btn-element";
+import useDesigner from "./hooks/use-designer";
+import FormElementsSidebar from "./form-elements-sidebar";
+import PropertiesFormSidebar from "./properties-form-sidebar";
 
 export default function DesignerSidebar() {
+  const { selectedElement } = useDesigner();
+
   return (
     <aside className="flex h-full w-[400px] max-w-[400px] flex-grow flex-col gap-2 overflow-y-auto border-l-2 border-muted bg-background p-4">
-      Elements
-      <SidebarBtnElement formElement={FormElements.TextField} />
+      {!selectedElement && <FormElementsSidebar />}
+      {selectedElement && <PropertiesFormSidebar />}
     </aside>
   );
 }

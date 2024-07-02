145377d775378482546bb67639e68e8158154948
Gyozadaigaku <gaspardcube15@gmail.com>
2024-07-02 14:26:23 +0900

    💄 Add `nextjs-toploader`

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "next-themes";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import DesignerContextProvider from "@/components/context/designer-context";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
       <html lang="en">
         <body className={cn(inter.className, "dark")}>
+          <NextTopLoader />
           <DesignerContextProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
               {children}

31fa6f655f71e06c223d4bb1c6db5e36554dcddd
Gyozadaigaku <gaspardcube15@gmail.com>
2024-07-02 15:58:55 +0900

    ✨ Add form submit page

---
+++ app/submit/[formUrl]/layout.tsx
@@ -0,0 +1,14 @@
+import Logo from "@/components/logo";
+import ThemeSwitcher from "@/components/theme-switcher";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen max-h-screen min-h-screen min-w-full flex-col bg-background">
+      <nav className="flex h-[60px] items-center justify-between border-b border-border px-4 py-2">
+        <Logo />
+        <ThemeSwitcher />
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+}

dd223a5517e1df397b2f8b23c7dc53951b710dd7
Gyozadaigaku <gaspardcube15@gmail.com>
2024-07-02 15:21:27 +0900

    ✨ Add form details page

---
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -0,0 +1,166 @@
+import { GetFormById, GetFormWithSubmissions } from "@/actions/form";
+import FormLinkShare from "@/components/form-link-share";
+import VisitBtn from "@/components/visit-btn";
+import { StatsCard } from "../../page";
+import { View, ClipboardList, MousePointerClick, Activity } from "lucide-react";
+import { ElementsType, FormElementInstance } from "@/components/form-elements";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { formatDistance } from "date-fns";
+
+async function FormDetailPage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const { id } = params;
+  const form = await GetFormById(Number(id));
+  if (!form) {
+    throw new Error("form not found");
+  }
+
+  const { visits, submissions } = form;
+
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bounceRate = 100 - submissionRate;
+
+  return (
+    <>
+      <div className="border-b border-muted py-10">
+        <div className="container flex justify-between">
+          <h1 className="truncate text-4xl font-bold">{form.name}</h1>
+          <VisitBtn shareUrl={form.shareURL} />
+        </div>
+      </div>
+      <div className="border-b border-muted py-4">
+        <div className="container flex items-center justify-between gap-2">
+          <FormLinkShare shareUrl={form.shareURL} />
+        </div>
+      </div>
+      <div className="container grid w-full grid-cols-1 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-4">
+        <StatsCard
+          title="Total visits"
+          icon={<View className="" />}
+          helperText="All time form visits"
+          value={visits.toLocaleString() || ""}
+          loading={false}
+          className="shadow-md"
+        />
+
+        <StatsCard
+          title="Total submissions"
+          icon={<ClipboardList className="" />}
+          helperText="All time form submissions"
+          value={submissions.toLocaleString() || ""}
+          loading={false}
+          className="shadow-md"
+        />
+
+        <StatsCard
+          title="Submission rate"
+          icon={<MousePointerClick className="" />}
+          helperText="Visits that result in form submission"
+          value={submissionRate.toLocaleString() + "%" || ""}
+          loading={false}
+          className="shadow-md"
+        />
+
+        <StatsCard
+          title="Bounce rate"
+          icon={<Activity className="" />}
+          helperText="Visits that leaves without interacting"
+          value={bounceRate.toLocaleString() + "%" || ""}
+          loading={false}
+          className="shadow-md"
+        />
+      </div>
+
+      <div className="container pt-10">
+        <SubmissionsTable id={form.id} />
+      </div>
+    </>
+  );
+}
+
+export default FormDetailPage;
+
+type Row = { [key: string]: string } & {
+  submittedAt: Date;
+};
+
+async function SubmissionsTable({ id }: { id: number }) {
+  const form = await GetFormWithSubmissions(id);
+
+  if (!form) {
+    throw new Error("form not found");
+  }
+
+  const formElements = JSON.parse(form.content) as FormElementInstance[];
+  const columns: {
+    id: string;
+    label: string;
+    required: boolean;
+    type: ElementsType;
+  }[] = [];
+
+  formElements.forEach((element) => {
+    switch (element.type) {
+      case "TextField":
+        columns.push({
+          id: element.id,
+          label: element.extraAttributes?.label,
+          required: element.extraAttributes?.required,
+          type: element.type,
+        });
+        break;
+      default:
+        break;
+    }
+  });
+
+  const rows: Row[] = [];
+  form.FormSubmissions.forEach((submission) => {
+    const content = JSON.parse(submission.content);
+    rows.push({
+      ...content,
+      submittedAt: submission.createdAt,
+    });
+  });
+
+  return (
+    <>
+      <h1 className="my-4 text-2xl font-bold">Submissions</h1>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns.map((column) => (
+                <TableHead key={column.id} className="uppercase">
+                  {column.label}
+                </TableHead>
+              ))}
+              <TableHead className="text-right uppercase text-muted-foreground">Submitted at</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell className="text-right text-muted-foreground">
+                  {formatDistance(row.submittedAt, new Date(), {
+                    addSuffix: true,
+                  })}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+}
---
+++ components/form-link-share.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Share2 } from "lucide-react";
+import { toast } from "./ui/use-toast";
+
+export default function FormLinkShare({ shareUrl }: { shareUrl: string }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // avoiding window not defined error
+  }
+
+  const shareLink = `${window.location.origin}/submit/${shareUrl}`;
+  return (
+    <div className="flex flex-grow items-center gap-4">
+      <Input value={shareLink} readOnly />
+      <Button
+        className="w-[250px]"
+        onClick={() => {
+          navigator.clipboard.writeText(shareLink);
+          toast({
+            title: "Copied!",
+            description: "Link copied to clipboard",
+          });
+        }}>
+        <Share2 className="mr-2 h-4 w-4" />
+        Share link
+      </Button>
+    </div>
+  );
+}

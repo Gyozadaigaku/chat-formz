f2f736f665350b63e66abff44639b7c7dac3565f
Gyozadaigaku <gaspardcube15@gmail.com>
2024-07-03 13:34:53 +0900

    ♻️ Change cell node based on type

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -5,7 +5,9 @@ import { StatsCard } from "../../page";
 import { View, ClipboardList, MousePointerClick, Activity } from "lucide-react";
 import { ElementsType, FormElementInstance } from "@/components/form-elements";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { formatDistance } from "date-fns";
+import { format, formatDistance } from "date-fns";
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
 
 async function FormDetailPage({
   params,
@@ -112,6 +114,11 @@ async function SubmissionsTable({ id }: { id: number }) {
   formElements.forEach((element) => {
     switch (element.type) {
       case "TextField":
+      case "NumberField":
+      case "TextAreaField":
+      case "DateField":
+      case "SelectField":
+      case "CheckboxField":
         columns.push({
           id: element.id,
           label: element.extraAttributes?.label,
@@ -170,5 +177,18 @@ async function SubmissionsTable({ id }: { id: number }) {
 
 function RowCell({ type, value }: { type: ElementsType; value: string }) {
   let node: React.ReactNode = value;
+
+  switch (type) {
+    case "DateField":
+      if (!value) break;
+      const date = new Date(value);
+      node = <Badge variant={"outline"}>{format(date, "dd/MM/yyyy")}</Badge>;
+      break;
+    case "CheckboxField":
+      const checked = value === "true";
+      node = <Checkbox checked={checked} disabled />;
+      break;
+  }
+
   return <TableCell>{node}</TableCell>;
 }
